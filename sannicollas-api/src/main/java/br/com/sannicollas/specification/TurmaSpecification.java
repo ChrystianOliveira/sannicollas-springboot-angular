284b01abcd3cc7cc18e4b033e38a18581ace9c65
Chrystian de Oliveira <chrystian.max@souunit.com.br>
2024-07-31 16:29:34 -0300

    melhorando turma specification, por turno

--- sannicollas-api/src/main/java/br/com/sannicollas/specification/TurmaSpecification.java
+++ sannicollas-api/src/main/java/br/com/sannicollas/specification/TurmaSpecification.java
@@ -9,10 +9,7 @@ import org.springframework.data.jpa.domain.Specification;
 public class TurmaSpecification {
 
     public static Specification<Turma> porTurno(Long id) {
-        if (id != null && id == 0) {
-            return null;
-        }
-        return (root, query, criteriaBuilder) -> id == null ? null : criteriaBuilder.equal(root.get("turno").get("id"), id);
+        return (root, query, criteriaBuilder) -> (id == null || id == 0) ? null : criteriaBuilder.equal(root.get("turno").get("id"), id);
     }
 
     public static Specification<Turma> porTurmaNaoVazia() {
